4e2e9e2c1414e5c1b4f234d050e42997b7c915ac
Swanand <swanandtyadav@gmail.com>
2022-07-23 13:48:36 +0530

    added git tuto

---
+++ GIT_PR/src/main/java/Git_mvn/GIT_PR/App.java
@@ -0,0 +1,13 @@
+package Git_mvn.GIT_PR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
